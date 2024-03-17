ba641dcb2ac3546964ced600d9e68c1570a1f877
Marcio Rayron <marcio_rayron@live.com>
2024-03-17 19:15:45 -0300

    adicionando elemento de sons na interacao com os botoes e reorganizando o codigo

--- app.js
+++ app.js
@@ -14,9 +14,34 @@ function descriptografarTexto(textoCriptografado) {
         .replace(/ufat/g, 'u');
 }
 
+function tocarSomDone() {
+    let audio = document.getElementById('audio-feito');
+    if (audio.readyState === 4) {
+        audio.currentTime = 0;
+        audio.volume = 0.5;
+        audio.play();
+    }
+    else {
+        console.log('Não foi possível reproduzir o áudio.');
+    }
+}
+
+function tocarSomError() {
+    let audio = document.getElementById('audio-error');
+    if (audio.readyState === 4) {
+        audio.currentTime = 0;
+        audio.volume = 0.5;
+        audio.play();
+    }
+    else {
+        console.log('Não foi possível reproduzir o áudio.');
+    }
+}
+
 function validarTexto(texto) {
     return /^[a-z\s]*$/.test(texto);
 }
+
 function exibirAviso() {
     let cxExibirTxt = document.getElementById('cx-exibir-txt');
     cxExibirTxt.classList.add('shaking');
@@ -24,6 +49,7 @@ function exibirAviso() {
         cxExibirTxt.classList.remove('shaking');
     }, 1000);
 }
+
 function validarEExibirAviso(texto) {
     if (!texto) {
         exibirAviso();
@@ -40,23 +66,43 @@ function validarEExibirAviso(texto) {
     return true;
 }
 
+function agrupaFuncoes() {
+    tocarSomDone();
+    exibirBotoes();
+    naoExibirImgGaroto();
+}
+
+let cxExibirTxt = document.getElementById('cx-exibir-txt');
+
+function definirTamanhoTexto(elemento) {
+    elemento.style.fontSize = '1.5rem';
+}
+
 document.getElementById('btn-copiar').addEventListener('click', function () {
     let textoCopiado = document.getElementById('cx-exibir-txt').textContent;
     navigator.clipboard.writeText(textoCopiado);
 
-    let cxExibirTxt = document.getElementById('cx-exibir-txt');
     cxExibirTxt.textContent = 'Copiado!';
-    cxExibirTxt.style.fontSize = '1rem';
+    definirTamanhoTexto(cxExibirTxt);
 
     document.getElementById('texto').value = textoCopiado;
+    tocarSomDone();
 });
 
-function exibirBotaoCopiar() {
+document.getElementById('btn-recarregar').addEventListener('click', function() {
+    tocarSomDone();
+    window.location.reload();
+   
+});
+
+function exibirBotoes() {
     let btnCopiar = document.getElementById('btn-copiar');
     btnCopiar.style.display = 'inline-block';
+    let btnRegarregar = document.getElementById('btn-recarregar');
+    btnRegarregar.style.display = 'inline-block';
 }
 
-function naoExibirImgGaroto(){
+function naoExibirImgGaroto() {
     let imgGaroto = document.getElementById('img-garoto');
     imgGaroto.style.display = 'none';
 }
@@ -64,23 +110,25 @@ function naoExibirImgGaroto(){
 document.getElementById('btn-crip').addEventListener('click', function () {
     let texto = document.getElementById('texto').value.trim();
     if (!validarEExibirAviso(texto)) {
+        tocarSomError();
         return;
     }
     let textoCriptografado = criptografarTexto(texto);
     document.getElementById('cx-exibir-txt').textContent = textoCriptografado;
-    exibirBotaoCopiar();
-    naoExibirImgGaroto();
+    definirTamanhoTexto(cxExibirTxt);
+    agrupaFuncoes();
 
 });
 
 document.getElementById('btn-desc').addEventListener('click', function () {
     let textoCriptografado = document.getElementById('texto').value.trim();
     if (!validarEExibirAviso(textoCriptografado)) {
+        tocarSomError();
         return;
     }
     let textoOriginal = descriptografarTexto(textoCriptografado);
     document.getElementById('cx-exibir-txt').textContent = textoOriginal;
-    exibirBotaoCopiar();
-    naoExibirImgGaroto();
+    definirTamanhoTexto(cxExibirTxt);
+    agrupaFuncoes();
 
 });
