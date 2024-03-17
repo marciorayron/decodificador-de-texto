dfcabd7830f2ab06cf88c4cbc5b3bca7a427af2b
Marcio Rayron <marcio_rayron@live.com>
2024-03-17 15:31:30 -0300

    função para criptografar/descriptografar

---
+++ app.js
@@ -0,0 +1,15 @@
+function criptografarTexto(texto) {
+    return texto.replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+}
+
+function descriptografarTexto(textoCriptografado) {
+    return textoCriptografado.replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+}
