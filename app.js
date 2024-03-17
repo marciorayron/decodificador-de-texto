55ae34279c5b4d7f34a352960f74535e84772505
Marcio Rayron <marcio_rayron@live.com>
2024-03-17 15:41:05 -0300

    função para verificar se o texto contém apenas letras minúsculas e sem acentos e função para animação de div em caso de apertar os botões e o textoarea estiver em vazio

--- app.js
+++ app.js
@@ -13,3 +13,29 @@ function descriptografarTexto(textoCriptografado) {
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
 }
+
+function validarTexto(texto) {
+    return /^[a-z\s]*$/.test(texto);
+}
+function exibirAviso() {
+    let cxExibirTxt = document.getElementById('cx-exibir-txt');
+    cxExibirTxt.classList.add('shaking');
+    setTimeout(function () {
+        cxExibirTxt.classList.remove('shaking');
+    }, 1000);
+}
+function validarEExibirAviso(texto) {
+    if (!texto) {
+        exibirAviso();
+        return false;
+    }
+    if (!validarTexto(texto)) {
+        let paragrafo = document.querySelector('.paragrafo p');
+        paragrafo.classList.add('vermelho');
+        setTimeout(function () {
+            paragrafo.classList.remove('vermelho');
+        }, 1000);
+        return false;
+    }
+    return true;
+}
